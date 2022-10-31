ac0db4cb57e540e73f85c750d85a18ec4be0afb3
Jeff Allen <jeffallen89@gmail.com>
2022-10-31 15:09:24 -0400

    choro styling, add wards and labels

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,7 +1,11 @@
 <script>
-    import VotingSubDivisions from '../data/vsd.geo.json';
+    
     import { onMount } from 'svelte';
     import mapboxgl from "mapbox-gl";
+    
+    import VotingSubDivisions from '../data/vsd.geo.json';
+    import Wards from '../data/wards.geo.json';
+    import WardPts from '../data/wardsPts.geo.json';
     
     mapboxgl.accessToken = 'pk.eyJ1Ijoic2Nob29sb2ZjaXRpZXMiLCJhIjoiY2w4c3h6M2tuMDIwazNuczU3bXA3ZXpvaSJ9.ShjnM8qiYP6yqz2PcAUBOg';
 
@@ -26,8 +30,8 @@
 			style: 'mapbox://styles/schoolofcities/cl9wy9gww000j15r7llrtlun3',
 			center: [-79.37, 43.715],
 			zoom: 10.5,
-			maxZoom: 16,
-			minZoom: 9,
+			maxZoom: 16.5,
+			minZoom: 8.5,
 			bearing: -17.1,
 			projection: 'globe',
 			scrollZoom: true,
@@ -42,6 +46,16 @@
                 'type': 'geojson',
                 'data': VotingSubDivisions
             });
+
+            map.addSource('Wards', {
+                'type': 'geojson',
+                'data': Wards
+            });
+            map.addSource('WardPts', {
+                'type': 'geojson',
+                'data': WardPts
+            });
+
 
             map.addLayer({
                 'id': 'VotingSubDivisionsFill',
@@ -62,9 +76,10 @@
                         candidates[candidate].breaks[3],
                         candidates[candidate].colours[4]
                     ], 
-                    'fill-opacity': 0.75
+                    'fill-opacity': 0.65
                 }
-            });
+            }, 'rail');
+
             map.addLayer({
                 'id': 'VotingSubDivisionsLine',
                 'type': 'line',
@@ -74,6 +89,56 @@
                     'line-color': '#fff',
                     'line-width': 1,
                     'line-opacity': 1
+                }
+            }, 'rail');
+
+            map.addLayer({
+                'id': 'WardsWhite',
+                'type': 'line',
+                'source': 'Wards',
+                'layout': {},
+                'paint': {
+                    'line-color': '#fff',
+                    'line-width': 4,
+                    'line-opacity': 1
+                }
+            }, 'rail');
+
+            map.addLayer({
+                'id': 'WardsBlack',
+                'type': 'line',
+                'source': 'Wards',
+                'layout': {},
+                'paint': {
+                    'line-color': '#000',
+                    'line-width': 2,
+                    'line-opacity': 1
+                }
+            }, 'rail');
+
+            map.addLayer({
+                'id': 'WardsLabel',
+                'type': 'symbol',
+                'source': 'WardPts',
+                'layout': {
+                    'text-field': ['get', 'name'],
+                    'text-size': 12,
+                    'text-transform': "uppercase",
+                    'text-justify': 'center',
+                    'text-allow-overlap': true,
+                },
+                'paint': {
+                    'text-halo-width': 1, 
+                    'text-halo-color': '#fff',
+                    'text-opacity': [
+                        "interpolate",
+                        ["linear"],
+                        ["zoom"],
+                        10.1,
+                        0,
+                        10.2,
+                        1
+                    ]
                 }
             });
         });
