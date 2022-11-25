1de4657b2bda403b3ddd665810a8370904a00faf
Jeff Allen <jeffallen89@gmail.com>
2022-11-25 15:59:37 -0500

    build, remove item from home

--- docs/_app/immutable/chunks/1-3e6ae7b7.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-9def290f.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-04fdd077.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-0df15927.js";import{default as t}from"../components/pages/_page.svelte-bdabb7e2.js";export{t as component,r as shared};
