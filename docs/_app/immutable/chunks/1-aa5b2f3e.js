d94dfcd79a15947e8502e88f8de241276154ad8e
Jeff Allen <jeffallen89@gmail.com>
2022-12-14 17:29:42 -0500

    build again, main web card links

--- docs/_app/immutable/chunks/1-aa5b2f3e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-bcff7bb5.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-2a87aa7d.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-0df15927.js";import{default as t}from"../components/pages/_page.svelte-6962d171.js";export{t as component,r as shared};
